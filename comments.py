fc93d68915c32bdd55a47d14cd7205c8cfc885cd
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-09-01 19:40:58 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,4 @@
+tank_size = 15 # This is the tank size
+distance = 600 # this is the distance
+mpg = distance / tank_size # This is the mpg that equals the distance divided nby the tank size
+print("Miles per gallon:", mpg) # this is explaining that mpg means miles per gallon
